c25bdf0374c498a99a45c90ce35d0b4009014f2d
seonghoonlim <hooney200@gmail.com>
2025-01-28 17:44:17 +0900

    #1 project setup and add appspec.yml

---
+++ src/main/java/dev/be/fastcampus/githubaction/FastcampusGithubactionApplication.java
@@ -0,0 +1,13 @@
+package dev.be.fastcampus.githubaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastcampusGithubactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastcampusGithubactionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/be/fastcampus/githubaction/FastcampusGithubactionApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.be.fastcampus.githubaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastcampusGithubactionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
